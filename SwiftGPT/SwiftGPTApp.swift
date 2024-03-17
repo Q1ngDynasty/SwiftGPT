721a024c2fdff1e0c62dde447504370fe76aff7f
Q1ngDynasty <freddyctl456@gmail.com>
2024-03-17 18:49:18 +0800

    Initial Commit

---
+++ SwiftGPT/SwiftGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftGPTApp.swift
+//  SwiftGPT
+//
+//  Created by ⠀Ubuntu ⠀ on 17/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
